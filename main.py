0e04154e11fd242f06873b578316017f976409f7
fishbulb1138 <dssanche@outlook.com>
2025-02-14 05:54:20 -0500

    Added player ship and left/right controls

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import pygame
 from constants import *
+from player import *
+from circleshape import *
+
+
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(x =SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
-        for eveint in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
+    
 
     
 
